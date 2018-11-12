51f701e1ebd7103df5bb93d949575252e1366d61
Lucas Martins Baia <lucasmbaia@hotmail.com>
2018-11-12 12:39:18 -0200

    added glide.yaml

--- etcd/etcd.go
+++ etcd/etcd.go
@@ -1,89 +1,89 @@
 package etcd
 
 import (
-  _etcd "github.com/coreos/etcd/client"
-  "context"
-  "time"
-  "encoding/json"
-  "reflect"
-  "errors"
+	"context"
+	"encoding/json"
+	"errors"
+	_etcd "github.com/etcd-io/etcd/client"
+	"reflect"
+	"time"
 )
 
 type Config struct {
-  Username  string
-  Password  string
-  Endpoints []string
-  Timeout   int32
+	Username  string
+	Password  string
+	Endpoints []string
+	Timeout   int32
 }
 
 type Client struct {
-  cli	  _etcd.KeysAPI
-  timeout int32
-  ctx	  context.Context
+	cli     _etcd.KeysAPI
+	timeout int32
+	ctx     context.Context
 }
 
 type Broker interface {
-  Set(key string, value interface{}) error
-  Get(key string, value interface{}) error
+	Set(key string, value interface{}) error
+	Get(key string, value interface{}) error
 }
 
 func NewClient(ctx context.Context, config Config) (Client, error) {
-  var (
-    client  _etcd.Client
-    err	    error
-  )
+	var (
+		client _etcd.Client
+		err    error
+	)
 
-  if client, err = _etcd.New(_etcd.Config{
-    Username:		      config.Username,
-    Password:		      config.Password,
-    Endpoints:		      config.Endpoints,
-    Transport:		      _etcd.DefaultTransport,
-    HeaderTimeoutPerRequest:  time.Duration(config.Timeout) * time.Second,
-  }); err != nil {
-    return Client{}, err
-  }
+	if client, err = _etcd.New(_etcd.Config{
+		Username:                config.Username,
+		Password:                config.Password,
+		Endpoints:               config.Endpoints,
+		Transport:               _etcd.DefaultTransport,
+		HeaderTimeoutPerRequest: time.Duration(config.Timeout) * time.Second,
+	}); err != nil {
+		return Client{}, err
+	}
 
-  return Client{
-    cli:      _etcd.NewKeysAPI(client),
-    timeout:  config.Timeout,
-    ctx:      ctx,
-  }, nil
+	return Client{
+		cli:     _etcd.NewKeysAPI(client),
+		timeout: config.Timeout,
+		ctx:     ctx,
+	}, nil
 }
 
 func (c Client) Set(key string, value interface{}) error {
-  var (
-    body  []byte
-    err	  error
-  )
+	var (
+		body []byte
+		err  error
+	)
 
-  if body, err = json.Marshal(value); err != nil {
-    return err
-  }
+	if body, err = json.Marshal(value); err != nil {
+		return err
+	}
 
-  if _, err = c.cli.Set(c.ctx, key, string(body), nil); err != nil {
-    return err
-  }
+	if _, err = c.cli.Set(c.ctx, key, string(body), nil); err != nil {
+		return err
+	}
 
-  return nil
+	return nil
 }
 
 func (c Client) Get(key string, value interface{}) error {
-  var (
-    err	error
-    response  *_etcd.Response
-  )
+	var (
+		err      error
+		response *_etcd.Response
+	)
 
-  if reflect.ValueOf(value).Kind() != reflect.Ptr {
-    return errors.New("Expected a pointer to a variable")
-  }
+	if reflect.ValueOf(value).Kind() != reflect.Ptr {
+		return errors.New("Expected a pointer to a variable")
+	}
 
-  if response, err = c.cli.Get(c.ctx, key, nil); err != nil {
-    return err
-  }
+	if response, err = c.cli.Get(c.ctx, key, nil); err != nil {
+		return err
+	}
 
-  if err = json.Unmarshal([]byte(response.Node.Value), value); err != nil {
-    return err
-  }
+	if err = json.Unmarshal([]byte(response.Node.Value), value); err != nil {
+		return err
+	}
 
-  return nil
+	return nil
 }
--- etcd/etcd_test.go
+++ etcd/etcd_test.go
@@ -1,63 +1,63 @@
 package etcd
 
 import (
-  "testing"
-  "context"
-  "fmt"
+	"context"
+	"fmt"
+	"testing"
 )
 
 func connect() (Client, error) {
-  return NewClient(context.Background(), Config{
-    Endpoints:	[]string{"http://127.0.0.1:2379"},
-    Timeout:	5,
-  })
+	return NewClient(context.Background(), Config{
+		Endpoints: []string{"http://192.168.204.128:2379"},
+		Timeout:   5,
+	})
 }
 
 func TestNewClient(t *testing.T) {
-  var err error
+	var err error
 
-  if _, err = NewClient(context.Background(), Config{
-    Endpoints:	[]string{"http://127.0.0.1:2379"},
-    Timeout:	5,
-  }); err != nil {
-    t.Fatal(err)
-  }
+	if _, err = NewClient(context.Background(), Config{
+		Endpoints: []string{"http://192.168.204.128:2379"},
+		Timeout:   5,
+	}); err != nil {
+		t.Fatal(err)
+	}
 }
 
 func TestSet(t *testing.T) {
-  var s = struct {
-    Name      string  `json:",omitempty"`
-    LastName  string  `json:",omitempty"`
-  }{"teste", "etcd"}
+	var s = struct {
+		Name     string `json:",omitempty"`
+		LastName string `json:",omitempty"`
+	}{"teste", "etcd"}
 
-  var client Client
-  var err error
+	var client Client
+	var err error
 
-  if client, err = connect(); err != nil {
-    t.Fatal(err)
-  }
+	if client, err = connect(); err != nil {
+		t.Fatal(err)
+	}
 
-  if err = client.Set("test-set", s); err != nil {
-    t.Fatal(err)
-  }
+	if err = client.Set("test-set", s); err != nil {
+		t.Fatal(err)
+	}
 }
 
 func TestGet(t *testing.T) {
-  var s = struct {
-    Name      string  `json:",omitempty"`
-    LastName  string  `json:",omitempty"`
-  }{}
+	var s = struct {
+		Name     string `json:",omitempty"`
+		LastName string `json:",omitempty"`
+	}{}
 
-  var client Client
-  var err error
+	var client Client
+	var err error
 
-  if client, err = connect(); err != nil {
-    t.Fatal(err)
-  }
+	if client, err = connect(); err != nil {
+		t.Fatal(err)
+	}
 
-  if err = client.Get("test-set", &s); err != nil {
-    t.Fatal(err)
-  }
+	if err = client.Get("test-set", &s); err != nil {
+		t.Fatal(err)
+	}
 
-  fmt.Println(s)
+	fmt.Println(s)
 }
